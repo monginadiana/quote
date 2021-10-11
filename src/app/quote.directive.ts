36bcbe97623be8bab6aed3a47ebd7da51082fecc
monginadiana <diana.ondieki@student.moringa.com>
2021-10-11 18:58:46 +0300

    changes in the quotedirective

--- src/app/quote.directive.ts
+++ src/app/quote.directive.ts
@@ -1,10 +1,11 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appQuote]'
 })
 export class QuoteDirective {
 
-  constructor() { }
-
+  constructor(private elem:ElementRef){
+    this.elem.nativeElement.style.background = "rgb(255, 0, 255)";
+    }
 }
